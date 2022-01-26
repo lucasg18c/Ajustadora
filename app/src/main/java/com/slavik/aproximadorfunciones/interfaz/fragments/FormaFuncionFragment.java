70b6f89f4d7612c9e219c43ee405c2a51cbc1494
Lucas Slavik <lucasg18c@gmail.com>
2022-01-26 18:43:51 -0300

    Dependencias actualizadas y agregadas para compose

--- app/src/main/java/com/slavik/aproximadorfunciones/interfaz/fragments/FormaFuncionFragment.java
+++ app/src/main/java/com/slavik/aproximadorfunciones/interfaz/fragments/FormaFuncionFragment.java
@@ -9,7 +9,6 @@ import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.LoadAdError;
 import com.google.android.gms.ads.rewarded.RewardItem;
 import com.google.android.gms.ads.rewarded.RewardedAd;
-import com.google.android.gms.ads.rewarded.RewardedAdCallback;
 import com.google.android.gms.ads.rewarded.RewardedAdLoadCallback;
 import com.slavik.aproximadorfunciones.R;
 import com.slavik.aproximadorfunciones.modelo.DatosGlobales;
@@ -17,7 +16,7 @@ import com.slavik.aproximadorfunciones.modelo.DatosGlobales;
 public class FormaFuncionFragment extends BaseFragment {
 
     RewardedAd rewardedAd;
-    RewardedAdCallback addCallback;
+    //RewardedAdCallback addCallback;
     RewardedAdLoadCallback adLoadCallback;
     AlertDialog dialog;
     boolean showAd = false, loadError = false, adVisto = false;
@@ -51,43 +50,43 @@ public class FormaFuncionFragment extends BaseFragment {
     }
 
     private void precargarAnuncio() {
-        rewardedAd.loadAd(new AdRequest.Builder().build(), adLoadCallback);
+        //rewardedAd.loadAd(new AdRequest.Builder().build(), adLoadCallback);
     }
 
     private void crearRewarded() {
-        rewardedAd = new RewardedAd(getContext(), getText(R.string.id_rewarded_auto).toString());
+//        rewardedAd = new RewardedAd(getContext(), getText(R.string.id_rewarded_auto).toString());
+//
+//        adLoadCallback = new RewardedAdLoadCallback() {
+//            @Override
+//            public void onRewardedAdLoaded() {
+//                if (showAd)
+//                    dialog.show();
+//                showAd = false;
+//                loadError = false;
+//            }
+//            @Override
+//            public void onRewardedAdFailedToLoad(LoadAdError adError) {
+//                loadError = true;
+//                System.err.println("ANUNCIO: " + adError.toString());
+//            }
+//        };
 
-        adLoadCallback = new RewardedAdLoadCallback() {
-            @Override
-            public void onRewardedAdLoaded() {
-                if (showAd)
-                    dialog.show();
-                showAd = false;
-                loadError = false;
-            }
-            @Override
-            public void onRewardedAdFailedToLoad(LoadAdError adError) {
-                loadError = true;
-                System.err.println("ANUNCIO: " + adError.toString());
-            }
-        };
-
-        addCallback = new RewardedAdCallback() {
-            @Override
-            public void onUserEarnedReward(@NonNull RewardItem rewardItem) {
-                adVisto = true;
-            }
-
-            @Override
-            public void onRewardedAdClosed() {
-                if (adVisto) {
-                    adVisto = false;
-                    mostrarResultado("automatic");
-                }
-                crearRewarded();
-            }
-        };
-        precargarAnuncio();
+//        addCallback = new RewardedAdCallback() {
+//            @Override
+//            public void onUserEarnedReward(@NonNull RewardItem rewardItem) {
+//                adVisto = true;
+//            }
+//
+//            @Override
+//            public void onRewardedAdClosed() {
+//                if (adVisto) {
+//                    adVisto = false;
+//                    mostrarResultado("automatic");
+//                }
+//                crearRewarded();
+//            }
+//        };
+//        precargarAnuncio();
     }
 
     private void mostrarResultado(String tag) {
@@ -96,26 +95,26 @@ public class FormaFuncionFragment extends BaseFragment {
     }
 
     private void crearDialogo() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
-        builder.setTitle( getText(R.string.ver_anuncio));
-        builder.setMessage(getText(R.string.opcion_requiere_anuncio));
-        builder.setPositiveButton(getText(R.string.aceptar), (dialogInterface, i) -> rewardedAd.show(getActivity(), addCallback));
-        builder.setNegativeButton(getText(R.string.cancelar), null);
-        dialog = builder.create();
+//        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+//        builder.setTitle( getText(R.string.ver_anuncio));
+//        builder.setMessage(getText(R.string.opcion_requiere_anuncio));
+//        builder.setPositiveButton(getText(R.string.aceptar), (dialogInterface, i) -> rewardedAd.show(getActivity(), addCallback));
+//        builder.setNegativeButton(getText(R.string.cancelar), null);
+//        dialog = builder.create();
     }
 
     private void mostrarAd() {
 
-        if (rewardedAd.isLoaded()) dialog.show();
-        else if (loadError){
-            mostrarResultado("automatic");
-            precargarAnuncio();
-//            toast(R.string.error_anuncio);
-        }
-        else{
-            toast(R.string.cargando_anuncio);
-            showAd = true;
-        }
+//        if (rewardedAd.isLoaded()) dialog.show();
+//        else if (loadError){
+//            mostrarResultado("automatic");
+//            precargarAnuncio();
+////            toast(R.string.error_anuncio);
+//        }
+//        else{
+//            toast(R.string.cargando_anuncio);
+//            showAd = true;
+//        }
     }
 
     private class Click implements View.OnClickListener{
